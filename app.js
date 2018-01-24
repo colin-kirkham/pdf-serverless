b77d32f5026dac158b47f9a90e2366f65b46edb5
Colin Kirkham <colin.kirkham@pb.com>
2018-01-24 10:46:27 +0000

    Added png serving.

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 const fs = require('fs')
 
-module.exports.handler = function (event, context, callback) {
+module.exports.pdfHandler = function (event, context, callback) {
   const data = fs.readFileSync('test.pdf')
+  const body = Buffer.from(data).toString('base64')
+  callback(null, {
+    isBase64Encoded: true,
+    statusCode: 200,
+    headers: { 'content-type': 'application/pdf', 'content-length': body.length },
+    body
+  })
+}
+
+module.exports.pngHandler = function (event, context, callback) {
+  const data = fs.readFileSync('test.png')
   const body = Buffer.from(data).toString('base64')
   callback(null, {
     isBase64Encoded: true,
